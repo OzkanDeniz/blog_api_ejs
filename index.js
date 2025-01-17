b5098dcde0d844ca979ba03e4527535956cfafeb
ozkan <ozkandnz09@gmail.com>
2025-01-17 23:32:13 +0300

    nodemon and npm run ui

--- index.js
+++ index.js
@@ -59,3 +59,4 @@ app.use(require("./src/middlewares/errorHandler"));
 app.listen(PORT, () => console.log("Running: http://127.0.0.1:" + PORT));
 
 // require("./src/helpers/sync2")();
+//! Başlangıçta projeyi ayağa kaldırmak için 2 tane terminal açarız birine nodemon, diğerine npm run ui yazıp çalıştırılır. 
