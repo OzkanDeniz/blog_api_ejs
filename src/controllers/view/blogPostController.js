0802db9036245c73d546a193cff4b6721fc50e86
ozkan <ozkandnz09@gmail.com>
2025-01-19 15:22:47 +0300

    components

--- src/controllers/view/blogPostController.js
+++ src/controllers/view/blogPostController.js
@@ -54,10 +54,7 @@ module.exports = {
       "blogCategoryId",
     ); // get Primary Data
 
-    res.status(200).send({
-      error: false,
-      result: data,
-    });
+    res.render("postRead")
   },
 
   update: async (req, res) => {
